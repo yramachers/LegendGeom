5b2681bfdfbda49f37054fc46b3f72cff95b232d
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2022-04-11 13:12:56 +0100

    [code] Ge PVs accessible as daughters of layer LV, only one level of daughters stored in LV.

--- LegendBaseline/L1000Baseline/testscript.py
+++ LegendBaseline/L1000Baseline/testscript.py
@@ -11,9 +11,9 @@ print(det.reg.logicalVolumeDict.keys())
 print('All physical volumes:')
 print(det.reg.physicalVolumeDict.keys())
 
+#lay = det.reg.logicalVolumeDict['cavernLV'] # all volumes in Cavern
 #lay = det.reg.logicalVolumeDict['ULArLV'] # all volumes in ULAr
 lay = det.reg.logicalVolumeDict['LayerLV'] # all volumes in Layer
-#lay = det.reg.logicalVolumeDict['cavernLV'] # all volumes in Cavern
 for pv in lay.daughterVolumes: # is list of physical Volumes
     print('Name: ',pv.name,' copy number: ',pv.copyNumber)
     print('Position: ',pv.position.eval())
